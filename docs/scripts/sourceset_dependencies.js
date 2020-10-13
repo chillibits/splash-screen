0e8ed6d7456f5872b4bf83a0ef1d47523b53b6da
marcauberer <marc.auberer@chillibits.com>
2020-10-13 06:55:25 +0000

    Deploying to gh-pages from  @ eb61570ebd50d5af6242e5c8c931133894d59f9f 🚀

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":splashscreen:dokkaHtml/debug": [], ":splashscreen:dokkaHtml/main": [], ":splashscreen:dokkaHtml/release": []}'
+sourceset_dependencies = '{":splashscreen:dokkaHtml/androidTestRelease": [], ":splashscreen:dokkaHtml/debug": [], ":splashscreen:dokkaHtml/main": [], ":splashscreen:dokkaHtml/release": []}'
